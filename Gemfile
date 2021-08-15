a2496f3b806cb608a4121a45a594d3783dcb2af5
sachio <sachio.engineer@gmail.com>
2021-08-15 11:21:48 +0900

    :sparkles: Add エラーメッセージ等日本語化

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'dotenv-rails'
 
 # User
 gem 'devise'
+gem 'devise-i18n'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, uniqueness: true
-  validates :email, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  before_validation { email.downcase! }
-  validates :password, length: { minimum: 6 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
