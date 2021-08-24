32509ccd833b92abc9d9ba32d8a29571b3a73318
sachio <sachio.engineer@gmail.com>
2021-08-24 13:33:16 +0900

    :sparkles: Add Maps JavaScript API

--- app/assets/javascripts/divesites.js
+++ app/assets/javascripts/divesites.js
@@ -1,7 +1,26 @@
+// sidebar
 $('.select-area').click(function(){
-  // console.log('クリックしたよ！')
+  console.log("クリックしたよ！")
   $(this).siblings('ul').slideToggle();
   $(this).siblings('i').toggleClass('before-click');
   $('.select-area').not($(this)).siblings('ul').slideUp();
   $('.select-area').not($(this)).siblings('i').addClass('before-click');
 });
+
+// MAP
+let map
+let geocoder
+
+function initMap(){
+  geocoder = new google.maps.Geocoder()
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7828, lng:-73.9653},
+    zoom: 12,
+  });
+
+  marker = new google.maps.Marker({
+    position:  {lat: 40.7828, lng:-73.9653},
+    map: map
+  });
+}
--- app/controllers/divesites_controller.rb
+++ app/controllers/divesites_controller.rb
@@ -2,8 +2,6 @@ class DivesitesController < ApplicationController
   def index
     @divesites = Divesite.all
     @areas = @divesites.pluck(:area).uniq
-
-    @kanto_divesites = @divesites.where(area: "関東")
   end
 
   def show
