5072aecc67ce20f981f723b51ec2c8aa2cfa5f19
sachio <sachio.engineer@gmail.com>
2021-09-05 01:07:00 +0900

    :sparkles: Add マイページAjax機能 #32

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
+    @favorites = current_user.favorites.all
+  end
+  def myfavorite
+    @favorites = current_user.favorites.all
+    respond_to do |format|
+      format.js { render :myfavorite }
+      format.html { redirect_to mypage_user_path(current_user.id) }
+    end
+  end
+  def mypost
+    @conditions = current_user.conditions.all.order(created_at: :DESC)
+    respond_to do |format|
+      format.js { render :mypost }
+      format.html { redirect_to mypage_user_path(current_user.id) }
+    end
   end
   private
   def user_params
