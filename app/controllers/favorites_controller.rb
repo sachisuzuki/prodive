61bb97d738064a6280ff73b1514930fee259fc2b
sachio <sachio.engineer@gmail.com>
2021-09-05 18:22:45 +0900

    :+1: Update マイページプロフィール部分のAjax追加 #32

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,20 @@
 class FavoritesController < ApplicationController
   def create
-    favorite =current_user.favorites.create(divesite_id: params[:divesite_id])
-    redirect_to divesite_url(favorite.divesite_id)
-    flash[:notice] = "#{favorite.divesite.name}をお気に入りダイブサイトに登録しました"
+    @favorite =current_user.favorites.create(divesite_id: params[:divesite_id])
+    @msg = "#{@favorite.divesite.name}をお気に入りダイブサイトに登録しました"
+    respond_to do |format|
+      format.js { render :like }
+      format.html { redirect_to divesite_url(@favorite.divesite_id) }
+    end
   end
   def destroy
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to divesite_url(favorite.divesite_id)
-    flash[:warning] = "#{favorite.divesite.name}をお気に入りダイブサイトから削除しました"
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    @msg = "#{@favorite.divesite.name}をお気に入りダイブサイトから削除しました"
+    respond_to do |format|
+      format.js { render :dislike }
+      format.html { redirect_to divesite_url(@favorite.divesite_id) }
+    end
+
+
   end
 end
