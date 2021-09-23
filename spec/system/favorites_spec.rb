025a0e4b92ae769951edadddccbfc435229e88b5
sachio <sachio.engineer@gmail.com>
2021-09-24 00:51:29 +0900

    :sparkles: Add SystemSpec/Favorite #57

---
+++ spec/system/favorites_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Favorites", type: :system do
+  describe 'お気に入りダイブサイト機能' do
+    let!(:user){ FactoryBot.create(:user) }
+    let!(:divesite){ FactoryBot.create(:divesite) }
+    before do
+      sign_in(user)
+    end
+    context 'ダイブサイト詳細ページのお気に入りアイコンを押してお気に入りに登録した場合' do
+      before do
+        visit divesite_path(divesite.id)
+        find('.fa-meh').click
+      end
+      it 'フラッシュメッセージに登録したと表示される' do
+        have_selector '.alert-info', text: "お気に入りダイブサイトに登録しました"
+      end
+      it 'マイページにお気に入りにしたダイブサイト名が表示される' do
+        visit mypage_user_path(user.id)
+        have_content divesite.name
+      end
+    end
+  end
+end
