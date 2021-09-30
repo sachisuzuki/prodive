c263c5e32f6a87e0ce9c19de1d99fc57c03dd0b6
sachio <sachio.engineer@gmail.com>
2021-09-30 10:02:05 +0900

    :books: development.rbのコメントアウト削除

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,41 +1,41 @@
-# require 'csv'
-# require 'securerandom'
+require 'csv'
+require 'securerandom'
 
-# # Users
-# 5.times do |n|
-#   name = Faker::JapaneseMedia::StudioGhibli.character
-#   email = Faker::Internet.email
-#   password = "password"
-# 	avatar = open("./public/samples/avt#{n}.jpg")
-#   uid = SecureRandom.uuid.tr('-', '')
-#   User.create!(name: name,
-#               email: email,
-#               password: password,
-#               avatar: avatar,
-#               uid: uid,
-#               )
-# end
-# User.create!(name: "admin_user",
-#             email: "admin@mail.com",
-#             password: "password",
-#             avatar: open("./public/images/default-avatar.png"),
-#             uid: SecureRandom.uuid.tr('-', ''),
-#             admin: true,
-#             )
+# Users
+5.times do |n|
+  name = Faker::JapaneseMedia::StudioGhibli.character
+  email = Faker::Internet.email
+  password = "password"
+	avatar = open("./public/samples/avt#{n}.jpg")
+  uid = SecureRandom.uuid.tr('-', '')
+  User.create!(name: name,
+              email: email,
+              password: password,
+              avatar: avatar,
+              uid: uid,
+              )
+end
+User.create!(name: "admin_user",
+            email: "admin@mail.com",
+            password: "password",
+            avatar: open("./public/images/default-avatar.png"),
+            uid: SecureRandom.uuid.tr('-', ''),
+            admin: true,
+            )
 
-# # Divesites
-# CSV.foreach('db/Divesite.csv', headers: true) do |row|
-#   Divesite.create(area: row[0].to_i,
-#                   zone: row[1].to_i,
-#                   name: row[2],
-#                   address: row[3],
-#                   phone: row[4],
-#                   service: row[5],
-#                   discription: row[6],
-#                   latitude: row[7],
-#                   longitude: row[8],
-#                   )
-# end
+# Divesites
+CSV.foreach('db/Divesite.csv', headers: true) do |row|
+  Divesite.create(area: row[0].to_i,
+                  zone: row[1].to_i,
+                  name: row[2],
+                  address: row[3],
+                  phone: row[4],
+                  service: row[5],
+                  discription: row[6],
+                  latitude: row[7],
+                  longitude: row[8],
+                  )
+end
 
 # Conditions
 5.times do |n|
