d0247b936c76df794f4cb30049f0aea3b04f2e16
sachio <sachio.engineer@gmail.com>
2021-10-12 19:27:28 +0900

    :+1: Update レスポンシブ対応 Users/mypage #87

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,22 @@
+// マイページのメニュー選択
 $('a.mymenu-link').click(function(){
   $('.mymenu-link').not($(this)).parents('.mymenu').removeClass('currently-selected');
   $(this).parents('.mymenu').addClass('currently-selected');
+});
+
+
+// マイページのサイドメニュースライド表示
+$('a.sidemenu-btn').click(function() {
+  $('.floating-board').removeClass('mobile').addClass('active');
+  $('.sidemenu-btn').addClass('after-click');
+});
+
+$('a.close-btn').click(function() {
+  $('.floating-board').removeClass('active').addClass('mobile');
+  $('.sidemenu-btn').removeClass('after-click');
+});
+
+$('a.mymenu-link').click(function() {
+  $('.floating-board').removeClass('active').addClass('mobile');
+  $('.sidemenu-btn').removeClass('after-click');
 });
