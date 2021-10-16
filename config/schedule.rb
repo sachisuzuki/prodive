d57667e9747239d0d2ffedd7a111776e3473535d
sachio <sachio.engineer@gmail.com>
2021-10-17 00:59:15 +0900

    Test cron設定デプロイテスト03 #95

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,6 @@ rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
 
-every :day, at: '22:45' do
+every :day, at: '06:30' do
   rake 'db:seed'
 end
